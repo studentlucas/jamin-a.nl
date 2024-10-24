a5537fa81bd518185eadd5869936cd6fee2c4c38
adruijter <adruijter@gmail.com>
2024-10-24 10:11:59 +0200

    runscript

---
+++ app/db/runDbScripts.sql
@@ -0,0 +1,8 @@
+-- C:\wamp64\bin\mysql\mysql9.0.1\bin\mysql -u root -p ''
+
+source C:/projects2024/jamin_A/app/db/createscript_jamin_a.sql
+source C:/projects2024/jamin_A/app/db/StoredProcedures/spCreateCountry.sql
+source C:/projects2024/jamin_A/app/db/StoredProcedures/spDeleteCountryById.sql
+source C:/projects2024/jamin_A/app/db/StoredProcedures/spGetAllCountries.sql
+source C:/projects2024/jamin_A/app/db/StoredProcedures/spSelectCountryById.sql
+source C:/projects2024/jamin_A/app/db/StoredProcedures/spUpdateCountryById.sql
